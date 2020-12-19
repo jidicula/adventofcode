49c3128d87eb7eac3703c22463f3add6e6c4dea1
jidicula <johanan.idicula@gmail.com>
2020-12-19 13:20:04 -0500

    [feat][input_read] Generalize to take filename

--- input_read.py
+++ input_read.py
@@ -1,6 +1,6 @@
-def input_read():
+def input_read(filename: str):
     input_list = []
-    with open("input.txt", "r") as f:
+    with open(filename, "r") as f:
         for line in f:
             input_list.append(line)
     return input_list
